31dc407305bc9961010144b76db22d592c0f61be
kshitijlohani <k.lohani01@gmail.com>
2014-01-28 18:50:22 -0500

    added slider and countdown

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+$(document).ready(function ($) {
+
+    $("#palPriceHidden").hide();
+    countdown();
+
+    setInterval(function () {
+        moveRight();
+    }, 3000);
+
+    var slideCount = $('#slider ul li').length;
+    var slideWidth = $('#slider ul li').width();
+    var slideHeight = $('#slider ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
+
+    $('#slider').css({ width: slideWidth, height: slideHeight });
+
+    $('#slider ul').css({ width: sliderUlWidth, marginLeft: -slideWidth });
+
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: -slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+});
+
+
 var displayChart = function (chartName) {
     var chart = new CanvasJS.Chart(chartName,
         {
@@ -22,9 +54,28 @@ var displayChart = function (chartName) {
 }
 
 
-var displayMainSlider = function(sliderName){
+var displayPalPricing = function () {
+    $("#palPriceHidden").slideUp("fast");
+    $("#palPriceHidden").slideDown("slow");
+
+}
 
 
+function countdown() {
+    var target_date = new Date("Feb 10, 2014").getTime();
+    var days, hours, minutes, seconds;
+    var countdown = document.getElementById("countdown");
+    setInterval(function () {
+        var current_date = new Date().getTime();
+        var seconds_left = (target_date - current_date) / 1000;
+        days = parseInt(seconds_left / 86400);
+        seconds_left = seconds_left % 86400;
+        hours = parseInt(seconds_left / 3600);
+        seconds_left = seconds_left % 3600;
+        minutes = parseInt(seconds_left / 60);
+        seconds = parseInt(seconds_left % 60);
+        countdown.innerHTML = days + "d, " + hours + "h, " + minutes + "m, " + seconds + "s";
+    }, 1000);
 }
 
 
